fe30fc57d536a3cf1e858c79646186f7c9be9355
quan-0811 <quan.nh081104@gmail.com>
2025-02-18 13:18:08 +0700

    Finished fundamental building modules

--- module/mbconv.py
+++ module/mbconv.py
@@ -1,12 +1,39 @@
 import torch
 import torch.nn as nn
+import torchinfo
 from typing import Tuple
+from module.squeeze_and_excitation import SqueezeAndExcitation
 
 class MBConv(nn.Module):
-    def __init__(self, input_channels: int, kernel_size: Tuple[int, int], expansion_factor: int):
+    def __init__(self, input_channels: int, output_channels: int, kernel_size: Tuple[int, int], expansion_ratio: int, se_ratio: float):
         super().__init__()
+
+        self.skip_connection = input_channels == output_channels
+        hidden_units = input_channels * expansion_ratio
+
         self.expansion = nn.Sequential(
-            nn.Conv2d(in_channels=input_channels, out_channels=input_channels*expansion_factor, kernel_size=(1, 1), stride=1),
-            nn.BatchNorm2d(num_features=input_channels*expansion_factor),
+            nn.Conv2d(in_channels=input_channels, out_channels=hidden_units, kernel_size=(1, 1), stride=1),
+            nn.BatchNorm2d(num_features=hidden_units),
             nn.SiLU()
         )
+        self.depth_wise = nn.Sequential(
+            nn.Conv2d(in_channels=hidden_units, out_channels=hidden_units, kernel_size=kernel_size, stride=1, padding="same", groups=hidden_units),
+            nn.BatchNorm2d(num_features=hidden_units)
+        )
+        self.squeeze_excitation = SqueezeAndExcitation(channels=hidden_units, channel_reduce_ratio=se_ratio)
+        self.projection = nn.Sequential(
+            nn.Conv2d(in_channels=hidden_units, out_channels=output_channels, kernel_size=(1, 1), stride=1),
+            nn.BatchNorm2d(num_features=output_channels)
+        )
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        identity = x
+        x = self.expansion(x)
+        x = self.depth_wise(x)
+        x = self.squeeze_excitation(x)
+        x = self.projection(x)
+        if self.skip_connection:
+            return x + identity
+        else:
+            return x
+
+
--- module/squeeze_and_excitation.py
+++ module/squeeze_and_excitation.py
@@ -3,15 +3,19 @@ import torch.nn as nn
 from module.global_avg_pooling import GlobalAvgPooling
 
 class SqueezeAndExcitation(nn.Module):
-    def __init__(self, channels: int, channel_reduce_ratio: int):
+    def __init__(self, channels: int, channel_reduce_ratio: float):
         super().__init__()
+
+        squeeze_units = int(channels * channel_reduce_ratio)
+
         self.squeeze = GlobalAvgPooling(output_size=(1, 1))
         self.excitation = nn.Sequential(
-            nn.Linear(in_features=channels, out_features=channels // channel_reduce_ratio),
+            nn.Linear(in_features=channels, out_features=squeeze_units),
             nn.SiLU(),
-            nn.Linear(in_features=channels // channel_reduce_ratio, out_features=channels),
+            nn.Linear(in_features=squeeze_units, out_features=channels),
             nn.Sigmoid()
         )
+
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         squeeze = self.squeeze(x)
         excitation = self.excitation(squeeze.squeeze())
