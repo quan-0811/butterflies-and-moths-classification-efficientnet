6aa6e45a6f971e45d3e6871f4eef0599ace5b055
quan-0811 <quan.nh081104@gmail.com>
2025-02-19 22:23:34 +0700

    Upload data loaders

--- data_setup.py
+++ data_setup.py
@@ -1,14 +1,18 @@
 import torchvision
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split
 
-def create_dataloaders(train_dir: str, valid_dir: str, test_dir: str, batch_size: int, transform):
+
+def create_dataloaders(train_dir: str, test_dir: str, batch_size: int, train_transform, valid_test_transform):
 
     train_dataset = torchvision.datasets.ImageFolder(root=train_dir,
-                                                     transform=transform)
-    valid_dataset = torchvision.datasets.ImageFolder(root=valid_dir,
-                                                     transform=transform)
+                                                     transform=train_transform)
     test_dataset = torchvision.datasets.ImageFolder(root=test_dir,
-                                                    transform=transform)
+                                                    transform=valid_test_transform)
+
+    train_size = int(0.8 * len(train_dataset))
+    valid_size = len(train_dataset) - train_size
+
+    _, valid_dataset = random_split(train_dataset, [train_size, valid_size])
 
     train_dataloader = DataLoader(dataset=train_dataset,
                                   batch_size=batch_size,
@@ -16,7 +20,8 @@ def create_dataloaders(train_dir: str, valid_dir: str, test_dir: str, batch_size
                                   pin_memory=True)
     valid_dataloader = DataLoader(dataset=valid_dataset,
                                   batch_size=batch_size,
-                                  shuffle=False)
+                                  shuffle=False,
+                                  pin_memory=True)
     test_dataloader = DataLoader(dataset=test_dataset,
                                  batch_size=batch_size,
                                  shuffle=False,
